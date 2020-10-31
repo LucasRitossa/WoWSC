8d6bc6a7f180a1d0bee93210894f3404e1341a19
lucasRitossa <22.lucas.ritossa@westg.org>
2020-10-31 00:56:49 -0400

    incomplete element awaits

--- scraper.js
+++ scraper.js
@@ -1,52 +1,58 @@
-const puppeteer = require("puppeteer");
+const puppeteer = require('puppeteer');
 
 (async () => {
   const userInfo = {
-    country: "us",
-    realm: "area-52",
-    name: "lukè",
+    country: 'us',
+    realm: 'area-52',
+    name: 'lukè',
   };
 
-  let pageUrl = `https://raider.io/characters/${userInfo.country}/${userInfo.realm}/${userInfo.name}#season=season-bfa-4`;
+  let pageUrl = `https://raider.io/characters/${userInfo.country}/${
+      userInfo.realm}/${userInfo.name}#season=season-bfa-4`;
 
-  let browser = await puppeteer.launch();
+  let browser = await puppeteer.launch({headless: false, slowMo: 250});
   let page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  });
 
-  await page.goto(pageUrl, { waitUntil: "networkidle2" });
+  await page.goto(pageUrl, {waitUntil: 'domcontentloaded'});
+  const watchDog = page.waitForFunction("document.querySelectorAll('.rio-striped').length > 0");
+  await watchDog;
 
-  //clicks open all dungeon panels
-  //for (let i = 0;i < await page.evaluate(() => document.querySelectorAll('tbody[class="rio-striped"]').length);i++) {
-  //	await page.click(await page.evaluate(() => document.querySelectorAll('tbody[class="rio-striped"]')[i]));
-  //}
+  for (let i = 0; i < 12; i++) {
+    let tbody = undefined;
+    let row = undefined;
+    //scraping fails here, doesnt 
+    await page.evaluate(() => {
+      tbody = document.querySelectorAll('.rio-striped')[i];
+      row = tbody.querySelectorAll('tr')[0];
+    });
 
-  //outputs all dungeon times once all panels have been clicked open
+    await page.click(row);
+    debugger;
+  };
 
-  //let allDungeonTimes = await page.evaluate(() => {
-  //		let dataOutput = {};
-  //		document
-  //			.querySelectorAll('tbody[class="rio-striped"]')
-  //			.forEach((el, index) => {
-  //				dataOutput[index] = el.innerText;
-  //			});
-  //		return dataOutput;
-  //	});
+  let allDungeonTimes = await page.evaluate(() => {
+    let dataOutput = {};
+    document.querySelectorAll('tbody[class="rio-striped"]') .forEach((el, index) => { dataOutput[index] = el.innerText; });
+    return dataOutput;
+  });
 
-  //outputs top time for each dungeon, works without opening tabs, also pareses string (incomplete)
   let topDungeonTimes = await page.evaluate(() => {
     let splitRecordsFull = [];
     for (let i = 0; i < 12; i++) {
-      let dungeonRecords = document.querySelectorAll(
-        'tbody[class="rio-striped"]'
-      )[i].innerText;
-      let splitRecords = dungeonRecords
-        .replaceAll("\t", " ")
-        .replaceAll("\n", "")
-        .split(" ");
-      splitRecordsFull[i] = splitRecords.filter((el) => el !== " ");
+      let dungeonRecords =
+          document.querySelectorAll('tbody[class="rio-striped"]')[i].innerText;
+      let splitRecords =
+          dungeonRecords.replaceAll('\t', ' ').replaceAll('\n', '').split(' ');
+      // add trim()
+      splitRecordsFull[i] = splitRecords.filter((el) => el !== ' ');
     }
     return splitRecordsFull;
   });
 
-  console.log(topDungeonTimes);
+  console.log(allDungeonTimes);
   await browser.close();
 })();
